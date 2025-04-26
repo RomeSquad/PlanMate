1fda648531235c7dbe226738b29c3170fa61ecb0
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-26 21:42:43 +0300

    init project

---
+++ src/main/kotlin/data/repository/ChangeHistoryRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package org.example.data.repository
+
+import org.example.logic.repository.ChangeHistoryRepository
+
+class ChangeHistoryRepositoryImpl : ChangeHistoryRepository
---
+++ src/main/kotlin/logic/entity/User.kt
@@ -0,0 +1,3 @@
+package org.example.logic.entity
+
+class User()
