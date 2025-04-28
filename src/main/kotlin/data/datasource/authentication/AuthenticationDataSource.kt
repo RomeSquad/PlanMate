0be193474157581fd6286fd2163a1340fc163e99
AmrNasser <amr931111@gmail.com>
2025-04-28 18:58:13 +0300

    make auth and csv data source

--- src/main/kotlin/data/datasource/authentication/AuthenticationDataSource.kt
+++ src/main/kotlin/data/datasource/authentication/AuthenticationDataSource.kt
@@ -1,4 +1,8 @@
 package org.example.data.datasource.authentication
 
+import org.example.logic.entity.User
+
 interface AuthenticationDataSource {
+    fun saveUser(user: User)
+    fun getUser(username: String): User
 }
--- src/main/kotlin/data/datasource/authentication/CsvAuthenticationDataSource.kt
+++ src/main/kotlin/data/datasource/authentication/CsvAuthenticationDataSource.kt
@@ -1,4 +1,33 @@
 package org.example.data.datasource.authentication
 
-class CsvAuthenticationDataSource {
+import org.example.logic.entity.Admin
+import org.example.logic.entity.Mate
+import org.example.logic.entity.User
+
+class CsvAuthenticationDataSource : AuthenticationDataSource {
+
+    val fileName: String = "user.csv"
+
+    val fileHeader: List<String> = listOf("username", "password", "role")
+
+
+    override fun saveUser(user: User) {
+        when (user) {
+            is Admin -> {
+
+            }
+
+            is Mate -> {
+
+            }
+
+            else -> {
+                throw Exception()
+            }
+        }
+    }
+
+    override fun getUser(username: String): User {
+        TODO()
+    }
 }
