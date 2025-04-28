01797502df98bc8e5d461b49853a714bcf9bee4f
AmrNasser <amr931111@gmail.com>
2025-04-28 18:55:39 +0300

    make user data class

--- src/main/kotlin/logic/entity/User.kt
+++ src/main/kotlin/logic/entity/User.kt
@@ -1,3 +1,31 @@
 package org.example.logic.entity
 
-class User()
+interface User {
+
+    val username: String
+    val password: String
+    val userRole: UserRole
+        get() = UserRole.ADMIN
+
+    fun canCreateProject(): Boolean
+}
+
+enum class UserRole {
+    ADMIN, MATE
+}
+
+
+data class Admin(
+    override val username: String,
+    override val password: String
+) : User {
+    override fun canCreateProject() = true
+}
+
+data class Mate(
+    override val username: String,
+    override val password: String,
+    override val userRole: UserRole = UserRole.MATE
+) : User {
+    override fun canCreateProject() = false
+}
