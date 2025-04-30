ff3c2455ee58933715ea30b8965143a992c7f525
Safa Thaaer <safathaaer@gmail.com>
2025-04-30 13:01:26 +0300

    define ProjectDataSource interface with basic project operations

--- src/main/kotlin/data/datasource/project/ProjectDataSource.kt
+++ src/main/kotlin/data/datasource/project/ProjectDataSource.kt
@@ -1,4 +1,10 @@
 package org.example.data.datasource.project
+import  org.example.logic.entity.Project
+
 
 interface ProjectDataSource {
+    fun insertProject(project: Project)
+    fun editProject(project: Project)
+    fun getProjectById(id: String): Project?
+    fun deleteProject(id: String)
 }
