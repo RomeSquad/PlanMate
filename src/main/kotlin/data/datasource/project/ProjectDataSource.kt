6b8090e5a5bac1ebaef8dcf5fbef9b7737d0ea6a
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-05-01 02:21:04 +0300

    implement save and read project data source from csv file

--- src/main/kotlin/data/datasource/project/ProjectDataSource.kt
+++ src/main/kotlin/data/datasource/project/ProjectDataSource.kt
@@ -6,8 +6,6 @@ import org.example.logic.entity.CreateProjectRequest
 import org.example.logic.entity.CreateProjectResponse
 
 interface ProjectDataSource {
-    fun editProject(project: Project)
-    fun getProjectById(id: Int): Project?
-    fun insertProject(projectRequest: CreateProjectRequest):Result<CreateProjectResponse>
     fun getAllProjects(): Result<List<Project>>
+    fun saveAllProjects(projects: List<Project>):Result<Unit>
 }
