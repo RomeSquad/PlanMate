de6b31ca5e73c8daf72c6a2d8aa5623e41687dcb
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-30 15:11:13 +0300

    add project data source and repo interfaces

--- src/main/kotlin/data/datasource/project/ProjectDataSource.kt
+++ src/main/kotlin/data/datasource/project/ProjectDataSource.kt
@@ -1,4 +1,10 @@
 package org.example.data.datasource.project
 
+import org.example.logic.entity.CreateProjectRequest
+import org.example.logic.entity.CreateProjectResponse
+import org.example.logic.entity.Project
+
 interface ProjectDataSource {
+    fun insertProject(projectRequest: CreateProjectRequest):Result<CreateProjectResponse>
+    fun getAllProjects(): Result<List<Project>>
 }
