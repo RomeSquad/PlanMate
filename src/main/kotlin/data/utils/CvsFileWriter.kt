b76861b4e65631beb6357e0f9162670b38a674b1
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-04-30 19:38:50 +0300

    add CsvFileReader and CvsFileWriter with implementation tests

--- src/main/kotlin/data/utils/CvsFileWriter.kt
+++ src/main/kotlin/data/utils/CvsFileWriter.kt
@@ -2,14 +2,7 @@ package org.example.data.utils
 
 import data.utils.CustomFile
 
-interface IFileExtensionChecker {
-    fun readCsv(
-        csvFile: CustomFile,
-        hasHeader: Boolean = true,
-        delimiter: Char? = ',',
-        skipEmptyLines: Boolean = true
-    ): List<List<String>>
-
+interface CvsFileWriter {
     fun writeCsv(
         csvFile: CustomFile,
         data: List<List<String>>?,
