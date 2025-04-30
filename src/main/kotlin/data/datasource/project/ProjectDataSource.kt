07b9c92ecffe637f0e4db85f88b51c3e23205635
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-30 16:00:48 +0300

    refactor:change get project by id input from String to Int.

--- src/main/kotlin/data/datasource/project/ProjectDataSource.kt
+++ src/main/kotlin/data/datasource/project/ProjectDataSource.kt
@@ -4,11 +4,10 @@ import  org.example.logic.entity.Project
 
 import org.example.logic.entity.CreateProjectRequest
 import org.example.logic.entity.CreateProjectResponse
-import org.example.logic.entity.Project
 
 interface ProjectDataSource {
     fun editProject(project: Project)
-    fun getProjectById(id: String): Project?
+    fun getProjectById(id: Int): Project?
     fun insertProject(projectRequest: CreateProjectRequest):Result<CreateProjectResponse>
     fun getAllProjects(): Result<List<Project>>
 }
