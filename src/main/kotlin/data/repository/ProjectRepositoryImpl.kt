07e1402ed97220e11d53a2d16901ba4fe2be8086
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-30 15:12:04 +0300

    add insert fun in project repository impl

--- src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
@@ -1,8 +1,33 @@
 package org.example.data.repository
 
+import org.example.data.datasource.authentication.AuthenticationDataSource
 import org.example.data.datasource.project.ProjectDataSource
+import org.example.logic.entity.CreateProjectRequest
+import org.example.logic.entity.CreateProjectResponse
+import org.example.logic.entity.Project
+import org.example.logic.entity.toProject
 import org.example.logic.repository.ProjectRepository
 
 class ProjectRepositoryImpl(
     private val projectDataSource: ProjectDataSource,
-) : ProjectRepository
+    private val userDataSource: AuthenticationDataSource
+) : ProjectRepository {
+
+    private var projects = mutableListOf<Project>()
+
+    init {
+        projects += getAllProjects().getOrThrow()
+    }
+
+    override fun insertProject(projectRequest: CreateProjectRequest): Result<CreateProjectResponse> {
+       return projectRequest.toProject(getLatestProjectId()).run {
+            projects.add(this)
+            Result.success(CreateProjectResponse(id))
+        }
+    }
+
+    override fun getAllProjects(): Result<List<Project>> {
+        return projectDataSource.getAllProjects()
+    }
+    private fun getLatestProjectId() = projects.lastOrNull()?.id ?: 0
+}
