242b4e1a3749df0a97de607c3ff1269a14b4720f
Safa Thaaer <safathaaer@gmail.com>
2025-04-30 13:04:06 +0300

    implement ProjectRepository using ProjectDataSource

--- src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ProjectRepositoryImpl.kt
@@ -1,8 +1,26 @@
 package org.example.data.repository
 
 import org.example.data.datasource.project.ProjectDataSource
+import org.example.logic.entity.Project
 import org.example.logic.repository.ProjectRepository
 
 class ProjectRepositoryImpl(
-    private val projectDataSource: ProjectDataSource,
-) : ProjectRepository
+    private val projectDataSource: ProjectDataSource
+) : ProjectRepository {
+
+   fun insertProject(project: Project) {
+
+    }
+
+     fun editProject(project: Project) {
+
+    }
+
+     fun deleteProject(id: String) {
+
+    }
+
+     fun getProjectById(id: String): Project? {
+        return null
+    }
+}
