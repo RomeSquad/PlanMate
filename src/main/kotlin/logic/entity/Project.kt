9c80079fb1bb21f968af9c0325274fbe3735ade6
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-30 15:59:39 +0300

    Refactor Project data class and add create project data classes
    - Change `Project` id type from `String` to `Int`.
    - Add `CreateProjectRequest` and `CreateProjectResponse` data classes.
    - Add function `toProject` to convert `CreateProjectRequest` to `Project`.
    - Add `description`, `changeHistory`, and `state` to `Project`.

--- src/main/kotlin/logic/entity/Project.kt
+++ src/main/kotlin/logic/entity/Project.kt
@@ -1,7 +1,35 @@
 package org.example.logic.entity
 
-data class Project (
-    val id: String,
+data class Project(
+    val id: Int,
     val name: String,
+    val description: String,
+    val changeHistory: List<ChangeHistory>,
+    val state: State
+)
+
+data class CreateProjectRequest(
+    val name: String,
+    val userId: Int,
+    val userName: String,
     val description: String
 )
+
+data class CreateProjectResponse(
+    val id: Int,
+)
+
+fun CreateProjectRequest.toProject(lastId: Int) =
+    Project(
+        id = lastId + 1,
+        name = name,
+        description = description,
+        changeHistory = listOf(
+            ChangeHistory(
+                /*TODO*/
+            )
+        ),
+        state = State(
+            /*TODO*/
+        )
+    )
