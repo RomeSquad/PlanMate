50da88ff3452cb5481400f37aa10d527803bdf38
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-30 20:22:17 +0300

    add Project data class

--- src/main/kotlin/logic/entity/Project.kt
+++ src/main/kotlin/logic/entity/Project.kt
@@ -1,4 +1,35 @@
 package org.example.logic.entity
 
-class Project {
-}
+data class Project(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val changeHistory: List<ChangeHistory>,
+    val state: State
+)
+
+data class CreateProjectRequest(
+    val name: String,
+    val userId: Int,
+    val userName: String,
+    val description: String
+)
+
+data class CreateProjectResponse(
+    val id: Int,
+)
+
+fun CreateProjectRequest.toProject(lastId: Int) =
+    Project(
+        id = lastId + 1,
+        name = name,
+        description = description,
+        changeHistory = listOf(
+            ChangeHistory(
+                /*TODO*/
+            )
+        ),
+        state = State(
+            /*TODO*/
+        )
+    )
