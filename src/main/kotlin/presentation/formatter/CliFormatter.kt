d9d031f6ae75c84eb4e67212f09b18ae02fa9a28
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-01 14:15:44 +0300

    add omit border feature

--- src/main/kotlin/presentation/formatter/CliFormatter.kt
+++ src/main/kotlin/presentation/formatter/CliFormatter.kt
@@ -15,27 +15,48 @@ class CliFormatter : Formatter {
         height: Int = 5,
         sidePaddingSize: Int = 1,
         topBottomPaddingSize: Int = 1,
+        omitRightSideBorder: Boolean = false,
+        omitLeftSideBorder: Boolean = false,
+        omitTopBorder: Boolean = false,
+        omitBottomBorder: Boolean = false
     ): String {
-        val messageLines = message.split("\n")
         val output = StringBuilder()
-        output.append(topBottomBorder(width + sidePaddingSize * 2))
-        for (index in 0 until height) {
-            output.append(SIDE_BORDER)
-            output.append(messageLines.get(index).take(width))
-            output.appendLine(SIDE_BORDER)
-        }
 
-        output.append(topBottomBorder(width + sidePaddingSize * 2))
+        if (!omitTopBorder) output.append(topBorder(width, sidePaddingSize, topBottomPaddingSize))
+
+        val messageLines = message.split("\n")
+        for (index in 0 until height) {
+            if (!omitLeftSideBorder) output.append(SIDE_BORDER)
+            output.append(messageLines.get(index).take(width))
+            if (!omitRightSideBorder) output.appendLine(SIDE_BORDER)
+        }
+        
+        if (!omitBottomBorder) output.append(bottomBorder(width, sidePaddingSize, topBottomPaddingSize))
+
         return output.toString()
     }
 
-    private fun topBottomBorder(width: Int): String {
-        return " " + TOP_BOTTOM_BORDER.repeat(width)
+    private fun topBorder(width: Int, sidePaddingSize: Int, topBottomPaddingSize: Int): String {
+        val topBottomBorder = StringBuilder()
+        topBottomBorder.append(TOP_BOTTOM_BORDER.repeat(width + sidePaddingSize * 2))
+        for (topPaddingIndex in 0 until topBottomPaddingSize) {
+            topBottomBorder.appendLine()
+        }
+        return topBottomBorder.toString()
+    }
+
+    private fun bottomBorder(width: Int, sidePaddingSize: Int, topBottomPaddingSize: Int): String {
+        val topBottomBorder = StringBuilder()
+        for (botPaddingIndex in 0 until topBottomPaddingSize) {
+            topBottomBorder.appendLine()
+        }
+        topBottomBorder.append(TOP_BOTTOM_BORDER.repeat(width + sidePaddingSize * 2))
+        return topBottomBorder.toString()
     }
 
     fun verticalLayout(messages: List<String>, width: Int = 20, height: Int = 1): String {
         val output = StringBuilder()
-        messages.map { output.append(rectangleLayout(it)) }
+        messages.map { output.appendLine(rectangleLayout(it)) }
         return output.toString()
     }
 
