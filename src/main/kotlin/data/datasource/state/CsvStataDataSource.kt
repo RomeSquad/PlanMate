5eb69332acebe771d2cdc7129fee211777d65682
Hossam Abozaid SAleh <hossamabozeidsaleh@gmail.com>
2025-05-02 14:30:20 +0300

    implement CsvStateDataSource with add,edit,delete,and read functions

--- src/main/kotlin/data/datasource/state/CsvStataDataSource.kt
+++ src/main/kotlin/data/datasource/state/CsvStataDataSource.kt
@@ -1,22 +1,60 @@
-package org.example.data.datasource.state
-
+import org.example.data.datasource.state.StateDataSource
 import org.example.logic.entity.State
 import java.io.File
 
-class CsvStateDataSource(val statefile :File) : StateDataSource {
+class CsvStateDataSource : StateDataSource {
+    private val csvFile = File("state.csv")
+
     override fun getAllStates(): List<State> {
-        TODO("Not yet implemented")
+        val allStates = mutableListOf<State>()
+
+        if (!csvFile.exists()) {
+            println("CSV file not found")
+            return allStates
+        }
+
+        csvFile.readLines().forEach { line ->
+            if (line.isNotBlank()) {
+                val newState = parseOneLine(line)
+                allStates.add(newState)
+            }
+        }
+
+        return allStates
     }
+
+    private fun parseOneLine(line: String): State {
+        val stateIndex = line.split(",")
+        return State(
+            projectId = stateIndex.get(0),
+            name = stateIndex.get(1)
+        )
+    }
+
     override fun addState(state: State) {
-        TODO("Not yet implemented")
+        csvFile.appendText("${state.projectId},${state.name}\n")
     }
 
     override fun editState(id: String) {
-        TODO("Not yet implemented")
+        val allStates = getAllStates().toMutableList()
+        val index = allStates.indexOfFirst { it.projectId == id }
+
+        if (index != -1) {
+            val newName = "EditedName"
+            allStates[index] = allStates[index].copy(name = newName)
+            saveAllStates(allStates)
+        }
     }
 
     override fun deleteState(id: String) {
-        TODO("Not yet implemented")
+        val updatedStates = getAllStates().filterNot { it.projectId == id }
+        saveAllStates(updatedStates)
+    }
+
+    private fun saveAllStates(states: List<State>) {
+        csvFile.writeText("")
+        states.forEach { state ->
+            csvFile.appendText("${state.projectId},${state.name}\n")
+        }
     }
 }
-//
