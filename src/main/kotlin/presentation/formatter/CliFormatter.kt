50b51960fdf68a238b9200344b7a196d02fd9156
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-02 04:46:10 +0300

    fix rectangle layout wrong format

--- src/main/kotlin/presentation/formatter/CliFormatter.kt
+++ src/main/kotlin/presentation/formatter/CliFormatter.kt
@@ -2,6 +2,7 @@ package org.example.presentation.formatter
 
 import org.koin.core.logger.MESSAGE
 import javax.swing.border.Border
+import kotlin.math.roundToInt
 
 
 class CliFormatter : Formatter {
@@ -11,8 +12,8 @@ class CliFormatter : Formatter {
      */
     fun rectangleLayout(
         message: String,
-        width: Int = 50,
-        height: Int = 5,
+        width: Int = 30,
+        height: Int = 3,
         sidePaddingSize: Int = 1,
         topBottomPaddingSize: Int = 1,
         omitRightSideBorder: Boolean = false,
@@ -26,12 +27,40 @@ class CliFormatter : Formatter {
 
         val messageLines = message.split("\n")
         for (index in 0 until height) {
-            if (!omitLeftSideBorder) output.append(SIDE_BORDER)
-            output.append(messageLines.get(index).take(width))
-            if (!omitRightSideBorder) output.appendLine(SIDE_BORDER)
+            if (index >= messageLines.size) {
+                if (!omitLeftSideBorder) output.append(SIDE_BORDER)
+                output.append(" ".repeat(width))
+                if (!omitRightSideBorder) output.append(SIDE_BORDER)
+                output.appendLine()
+            } else {
+                if (!omitLeftSideBorder) output.append(SIDE_BORDER)
+
+                if (messageLines[index].length + sidePaddingSize * 2 > width) {
+                    output.append(messageLines[index].take(width))
+                } else {
+
+                    val empty = ((width - messageLines[index].length - sidePaddingSize * 2) / 2) + 1
+
+                    var emptySpace = " ".repeat(empty)
+
+                    output.append(emptySpace)
+                    output.append(messageLines[index])
+                    if (Math.floorMod(messageLines[index].length, 2) != 0) output.append(" ")
+                    output.append(emptySpace)
+                }
+                if (!omitRightSideBorder) output.append(SIDE_BORDER)
+                output.appendLine()
+            }
         }
 
-        if (!omitBottomBorder) output.append(bottomBorder(width, sidePaddingSize, topBottomPaddingSize))
+        if (!omitBottomBorder) {
+            output.append(
+                bottomBorder(
+                    width, sidePaddingSize, topBottomPaddingSize,
+                    omitRightSideBorder, omitLeftSideBorder
+                )
+            )
+        }
 
         return output.toString()
     }
@@ -45,24 +74,49 @@ class CliFormatter : Formatter {
         return topBottomBorder.toString()
     }
 
-    private fun bottomBorder(width: Int, sidePaddingSize: Int, topBottomPaddingSize: Int): String {
+    private fun bottomBorder(
+        width: Int, sidePaddingSize: Int, topBottomPaddingSize: Int, omitRightSideBorder: Boolean = false,
+        omitLeftSideBorder: Boolean = false,
+    ): String {
         val topBottomBorder = StringBuilder()
         for (botPaddingIndex in 0 until topBottomPaddingSize) {
+            if (!omitLeftSideBorder) topBottomBorder.append(SIDE_BORDER)
+            topBottomBorder.append(" ".repeat(width))
+            if (!omitRightSideBorder) topBottomBorder.append(SIDE_BORDER)
             topBottomBorder.appendLine()
         }
         topBottomBorder.append(TOP_BOTTOM_BORDER.repeat(width + sidePaddingSize * 2))
         return topBottomBorder.toString()
     }
 
-    fun verticalLayout(messages: List<String>, width: Int = 20, height: Int = 1): String {
+    fun verticalLayout(messages: List<String>, width: Int = 30, height: Int = 3): String {
+
         val output = StringBuilder()
-        messages.map { output.appendLine(rectangleLayout(it)) }
+        messages.map {
+            output.append(
+                rectangleLayout(
+                    message = it,
+                    width = width,
+                    height = height,
+                    omitRightSideBorder = true
+                )
+            )
+        }
         return output.toString()
     }
 
-    fun horizontalLayout(messages: List<String>, width: Int = 20, height: Int = 1): String {
+    fun horizontalLayout(messages: List<String>, width: Int = 30, height: Int = 3): String {
         val output = StringBuilder()
-        messages.map { output.appendLine(rectangleLayout(it)) }
+        messages.map {
+            output.appendLine(
+                rectangleLayout(
+                    message = it,
+                    width = width,
+                    height = height,
+                    omitBottomBorder = true
+                )
+            )
+        }
         return output.toString()
     }
 
