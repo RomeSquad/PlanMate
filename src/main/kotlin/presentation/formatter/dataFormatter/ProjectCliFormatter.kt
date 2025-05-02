e5d87b9794e14fc15cd01aea9106b5d9114fd4f5
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-02 05:05:35 +0300

    show project change history

--- src/main/kotlin/presentation/formatter/dataFormatter/ProjectCliFormatter.kt
+++ src/main/kotlin/presentation/formatter/dataFormatter/ProjectCliFormatter.kt
@@ -1,17 +1,40 @@
 package org.example.presentation.formatter.dataFormatter
 
+import org.example.logic.entity.ChangeHistory
 import org.example.logic.entity.Project
+import org.example.logic.entity.State
+import org.example.presentation.formatter.CliFormatter
 
 
 fun Project.format(): String {
-    return """
-            Project Name: $name
-            Project Description: $description
-        """.trimIndent()
+    return String.format(
+        "%-10s | %-20s | %-20s | %-30s | %-15s | %-15s | %-10s | %-10s",
+        "ID: $id",
+        "Name: $name",
+        "Description: $description",
+        "State: $state",
+        "Change History: $changeHistory"
+    )
 }
 
+fun Project.changeHistoryFormat(): String {
+    val cliFormatter = CliFormatter()
+    return String.format(
+        "%-10s | %-20s | %-20s \n%-30s \n",
+        "ID: $id",
+        "Name: $name",
+        "State: $state",
+        "Description: $description"
+    ) + cliFormatter.verticalLayout(changeHistory.map { it.format() }.toList())
+}
 
 fun Project.swimlaneFormat(): String {
-    TODO()
+    return String.format(
+        "%-10s | %-20s | %-20s \n%-30s \n%-15s",
+        "ID: $id",
+        "Name: $name",
+        "State: $state",
+        "Description: $description"
+    )
 }
 
