98856884f50c79a8f10363824fecb7719a43c350
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-05-02 16:17:30 +0300

    override getHistoryByProjectID function in ChangeHistoryRepositoryImpl class

--- src/main/kotlin/data/repository/ChangeHistoryRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ChangeHistoryRepositoryImpl.kt
@@ -1,5 +1,14 @@
 package org.example.data.repository
 
+import org.example.data.datasource.project.ProjectDataSource
+import org.example.logic.entity.Project
 import org.example.logic.repository.ChangeHistoryRepository
 
-class ChangeHistoryRepositoryImpl : ChangeHistoryRepository
+class ChangeHistoryRepositoryImpl(private val dataSource: ProjectDataSource
+) : ChangeHistoryRepository {
+
+    override fun getHistoryByProjectId(projectId: Int): Result<List<Project>> {
+        TODO("Not yet implemented")
+    }
+
+}
