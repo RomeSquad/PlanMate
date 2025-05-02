3ebed6b224c9deb70bfa4063a15abe7f7d0bc95a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-02 18:17:40 +0300

    fix getActions Error

--- src/main/kotlin/presentation/menus/Menu.kt
+++ src/main/kotlin/presentation/menus/Menu.kt
@@ -1,10 +1,9 @@
 package org.example.presentation.menus
 
-import org.koin.java.KoinJavaComponent.getKoin
 import presentation.App
 
 open class Menu(
-    open val actions: List<MenuAction>,
+    val actions: List<MenuAction>,
     val app: App
 ) {
 
@@ -13,7 +12,7 @@ open class Menu(
         return actions[index - 1]
     }
 
-    fun getActions(): List<MenuAction> = actions
+    fun getActionsList(): List<MenuAction> = actions
 
     /*
      *  You can use koin to get the menu you need. Example:
