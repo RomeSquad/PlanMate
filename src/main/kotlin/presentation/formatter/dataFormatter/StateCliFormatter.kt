926ad7f8169b6a1ff3e8a3d8a4e1d23d0e877c41
Arzo-zi <zinahashraf88@gmail.com>
2025-05-03 00:33:21 +0300

    fix parameter name

--- src/main/kotlin/presentation/formatter/dataFormatter/StateCliFormatter.kt
+++ src/main/kotlin/presentation/formatter/dataFormatter/StateCliFormatter.kt
@@ -3,6 +3,6 @@ package org.example.presentation.formatter.dataFormatter
 import org.example.logic.entity.State
 
 fun State.format(): String {
-    return String.format("%-20s | %-20s", "Project ID: $projectId", "State: $name")
+    return String.format("%-20s | %-20s", "Project ID: $projectId", "State: $stateName")
 }
 
