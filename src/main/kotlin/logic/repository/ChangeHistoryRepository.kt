b9d5c251b5f258a9f50fefc32caed68d7081183e
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-05-02 16:14:02 +0300

    add function getHistory by projectID to ChangeHistoryRepository interface

--- src/main/kotlin/logic/repository/ChangeHistoryRepository.kt
+++ src/main/kotlin/logic/repository/ChangeHistoryRepository.kt
@@ -1,4 +1,7 @@
 package org.example.logic.repository
 
+import org.example.logic.entity.Project
+
 interface ChangeHistoryRepository {
+        fun getHistoryByProjectId(projectId: Int): Result<List<Project>>
 }
