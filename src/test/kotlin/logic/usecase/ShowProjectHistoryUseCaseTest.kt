85234b0ee3cbe88ef6b1fe23f06ee429bc3e1741
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-05-06 17:58:35 +0300

    write ShowProjectHistoryUseCase test before implementation

---
+++ src/test/kotlin/logic/usecase/ShowProjectHistoryUseCaseTest.kt
@@ -0,0 +1,66 @@
+package logic.usecase
+
+import io.mockk.every
+import io.mockk.mockk
+import org.example.logic.entity.ChangeHistory
+import org.example.logic.entity.Project
+import org.example.logic.entity.State
+import org.example.logic.repository.ChangeHistoryRepository
+import org.example.logic.usecase.ShowProjectHistoryUseCase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.sql.Date
+import kotlin.test.assertEquals
+
+class ShowProjectHistoryUseCaseTest {
+
+    private lateinit var changeHistoryRepository: ChangeHistoryRepository
+    private lateinit var useCase: ShowProjectHistoryUseCase
+
+    @BeforeEach
+    fun setup() {
+        changeHistoryRepository = mockk()
+        useCase = ShowProjectHistoryUseCase(changeHistoryRepository)
+    }
+
+    @Test
+    fun `should return project change history when project ID exist`() {
+
+        //given
+        val Id = 12355
+        every { useCase.excute(Id) } returns Result.success(FakeProjectData())
+
+        //when
+        val result = useCase.excute(Id)
+
+        //then
+        assertEquals(result.getOrNull(), FakeProjectData())
+    }
+    @Test
+    fun`should return failure when the project id is not exist`(){
+
+        //given
+        val ID=11111
+        every { useCase.excute(ID) }returns Result.failure( NoSuchElementException("Project 11111 not found"))
+
+        //when
+        val result=useCase.excute(ID)
+
+        //then
+        assertEquals(NoSuchElementException(), result.exceptionOrNull())  }
+
+
+    //helper
+    fun FakeProjectData(): Project {
+        val fixedDate = Date(12355)
+        return Project(
+            id = 123,
+            name = "ahmed",
+            description = "Testing history",
+            changeHistory = listOf(
+                ChangeHistory("1", "T1", "U1", fixedDate, "Created project")
+            ),
+            state = State("1", "InProgress")
+        )
+    }
+}
