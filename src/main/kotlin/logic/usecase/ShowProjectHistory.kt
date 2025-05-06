f4109eb405928b3f1bf3320eca750331f853b19d
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-05-06 17:11:04 +0300

    write class ShowProjectHistory header before test

---
+++ src/main/kotlin/logic/usecase/ShowProjectHistory.kt
@@ -0,0 +1,8 @@
+package org.example.logic.usecase
+
+import org.example.logic.repository.ChangeHistoryRepository
+
+class ShowProjectHistory(
+    private val repository: ChangeHistoryRepository
+)
+
