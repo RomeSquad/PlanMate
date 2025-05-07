693207fec425202d5f5e64c14a02c8eec6a1bb46
fatima faisal <fatima.faisal.ghazi@gmail.com>
2025-05-07 16:23:54 +0300

    implement ShowProjectHistoryUseCase

--- src/main/kotlin/logic/usecase/ShowProjectHistoryUseCase.kt
+++ src/main/kotlin/logic/usecase/ShowProjectHistoryUseCase.kt
@@ -1,8 +1,13 @@
 package org.example.logic.usecase
 
+import org.example.logic.entity.Project
 import org.example.logic.repository.ChangeHistoryRepository
 
 class ShowProjectHistoryUseCase(
     private val repository: ChangeHistoryRepository
-)
+) {
+    fun execute(projectId: Int): Result<Project> {
+        return repository.getHistoryByProjectId(projectId)
+    }
+}
 
