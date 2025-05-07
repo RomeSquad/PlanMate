ebf791de89595177af10f4045c22439db5741f9a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-08 02:28:32 +0300

    fix : change author id type to int

--- src/main/kotlin/logic/entity/ChangeHistory.kt
+++ src/main/kotlin/logic/entity/ChangeHistory.kt
@@ -5,7 +5,7 @@ import java.util.Date
 data class ChangeHistory(
     val projectID: Int,
     val taskID: String,
-    val authorID: String,
+    val authorID: Int,
     val changeDate: Date,
     val changeDescription: String
 )
