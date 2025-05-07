f262fabb556f4713a4f9a0076eacbb4333940fef
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-05-08 02:29:00 +0300

    feat : add change history

---
+++ src/main/kotlin/logic/usecase/history/AddChangeHistoryUseCase.kt
@@ -0,0 +1,23 @@
+package org.example.logic.usecase.history
+
+import org.example.logic.entity.ChangeHistory
+import org.example.logic.repository.ChangeHistoryRepository
+import java.util.Date
+
+class AddChangeHistoryUseCase(private val repository: ChangeHistoryRepository) {
+
+    suspend fun execute(projectId: Int, taskId: String, authorId: Int, changeDate: Date, changeDescription: String): ChangeHistory {
+        try {
+            val changeHistory = ChangeHistory(
+                projectID = projectId,
+                taskID = taskId,
+                authorID = authorId,
+                changeDate = changeDate,
+                changeDescription = changeDescription
+            )
+            return repository.addChangeHistory(changeHistory)
+        } catch (e: Exception) {
+            throw IllegalArgumentException("Invalid Change History data: ${e.message}")
+        }
+    }
+}
