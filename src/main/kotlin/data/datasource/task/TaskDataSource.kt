145e3cfa0d1a8074b98a9f45e6ff317a4741780d
mohamed <mohamed.ramadan19502@gmail.com>
2025-05-08 12:16:45 +0300

    convert functions to suspend functions

--- src/main/kotlin/data/datasource/task/TaskDataSource.kt
+++ src/main/kotlin/data/datasource/task/TaskDataSource.kt
@@ -3,13 +3,13 @@ package org.example.data.datasource.task
 import org.example.logic.entity.Task
 
 interface TaskDataSource {
-    fun createTask(task: Task)
-    fun editTask(taskId: String, title: String, description: String, updatedAt: Long)
-    fun deleteTask(projectId: Int, taskId: String)
+    suspend fun createTask(task: Task)
+    suspend fun editTask(taskId: String, title: String, description: String, updatedAt: Long)
+    suspend fun deleteTask(projectId: Int, taskId: String)
 
-    fun getTaskByIdFromFile(taskId: String): Task
-    fun getTasksByProjectId(projectId: Int): List<Task>
+    suspend fun getTaskByIdFromFile(taskId: String): Task
+    suspend fun getTasksByProjectId(projectId: Int): List<Task>
 
-    fun getAllTasks(): List<Task>
-    fun saveAllTasks(tasks: List<Task>)
+    suspend fun getAllTasks(): List<Task>
+    suspend fun saveAllTasks(tasks: List<Task>)
 }
