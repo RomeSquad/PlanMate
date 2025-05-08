48bf184a192946899e9db7910c0b18509bf45852
Arzo-zi <zinahashraf88@gmail.com>
2025-05-08 12:55:47 +0300

    refactor AddStatesUseCase and its test

--- src/main/kotlin/logic/usecase/state/AddStatesUseCase.kt
+++ src/main/kotlin/logic/usecase/state/AddStatesUseCase.kt
@@ -3,11 +3,11 @@ package org.example.logic.usecase.state
 import org.example.logic.entity.State
 import org.example.logic.repository.StateRepository
 
-class AddStatesUseCase(private val stateRepository: StateRepository) {
-    fun executeAddState(stateName: String, projectId: String): Boolean {
+class AddStatesUseCase(
+    private val stateRepository: StateRepository
+) {
+    fun execute(stateName: String, projectId: Int) {
         require(stateName.isNotBlank()) { "State Name must not be blank" }
-        require(projectId.isNotBlank()) { "project id must not be blank" }
-
 
         val state = State(
             stateName = stateName,
@@ -15,6 +15,5 @@ class AddStatesUseCase(private val stateRepository: StateRepository) {
         )
 
         stateRepository.addState(state)
-        return true
     }
 }
--- src/test/kotlin/logic/usecase/state/AddStatesUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/state/AddStatesUseCaseTest.kt
@@ -1,13 +1,11 @@
 package logic.usecase.state
 
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import org.example.logic.repository.StateRepository
 import org.example.logic.usecase.state.AddStatesUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import kotlin.test.assertTrue
 
 class AddStatesUseCaseTest {
     private lateinit var stateRepository: StateRepository
@@ -22,24 +20,18 @@ class AddStatesUseCaseTest {
     @Test
     fun ` should throw exception when state name is blank`() {
         assertThrows<IllegalArgumentException> {
-            addStatesUseCase.executeAddState("", "1")
-        }
-    }
-
-    @Test
-    fun ` should throw exception when project id is blank`() {
-        assertThrows<IllegalArgumentException> {
-            addStatesUseCase.executeAddState("pending", "")
+            addStatesUseCase.execute("", 1)
         }
     }
 
     @Test
     fun ` should add state to project successfully`() {
-        every { stateRepository.addState(any()) } returns true
-        val result = addStatesUseCase.executeAddState("todo", "2")
+        every { stateRepository.addState(any()) } just Runs
 
-        assertTrue { result }
+        addStatesUseCase.execute("todo", 2)
+
+        verify(exactly = 1) { stateRepository.addState(any()) }
     }
 
-
 }
+
