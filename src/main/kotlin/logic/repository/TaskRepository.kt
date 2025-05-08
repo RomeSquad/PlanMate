5597c4e4d5fc9d12961107b0877ce3989e10cb90
mohamed <mohamed.ramadan19502@gmail.com>
2025-05-08 12:59:22 +0300

    handle code to fits kotlin coroutines in test and implementation

--- src/main/kotlin/logic/repository/TaskRepository.kt
+++ src/main/kotlin/logic/repository/TaskRepository.kt
@@ -3,12 +3,12 @@ package org.example.logic.repository
 import org.example.logic.entity.Task
 
 interface TaskRepository {
-    fun createTask(task: Task)
-    fun editTask(taskId: String, title: String, description: String, updatedAt: Long)
-    fun deleteTask(projectId: Int, taskId: String)
+    suspend fun createTask(task: Task)
+    suspend fun editTask(taskId: String, title: String, description: String, updatedAt: Long)
+    suspend fun deleteTask(projectId: Int, taskId: String)
 
-    fun getTaskById (taskId: String): Task
-    fun getTasksByProject(projectId: Int): List<Task>
+    suspend fun getTaskById (taskId: String): Task
+    suspend fun getTasksByProject(projectId: Int): List<Task>
 
-    fun getAllTasks(): List<Task>
+    suspend fun getAllTasks(): List<Task>
 }
