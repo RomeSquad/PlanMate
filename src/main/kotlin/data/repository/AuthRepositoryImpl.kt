51e945edb3334931dd84a9bf805f53065d20caea
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-05-11 14:35:20 +0300

    implement auth repository using auth datasource

--- src/main/kotlin/data/repository/AuthRepositoryImpl.kt
+++ src/main/kotlin/data/repository/AuthRepositoryImpl.kt
@@ -1,82 +1,26 @@
 package org.example.data.repository
 
-import kotlinx.coroutines.runBlocking
+import logic.request.auth.CreateUserRequest
 import org.example.data.datasource.authentication.AuthDataSource
 import org.example.logic.entity.auth.User
-import org.example.logic.entity.auth.UserRole
 import org.example.logic.repository.AuthRepository
-import org.example.utils.hashPassword
+import org.example.logic.request.auth.LoginRequest
 
 class AuthRepositoryImpl(
     private val authDataSource: AuthDataSource
 ) : AuthRepository {
 
-    private var users = mutableListOf<User>()
+    override suspend fun insertUser(request: CreateUserRequest): User = authDataSource.insertUser(request)
 
-    init {
-        runBlocking {
-            users += getAllUsers()
-        }
-    }
+    override suspend fun loginUser(request: LoginRequest): User =  authDataSource.loginUser(request)
 
-    override suspend fun insertUser(username: String, password: String, userRole: UserRole): User {
-        checkUserNameAndPassword(username, password)
-        validPassword(password)
-
-        val hashedPassword = hashPassword(password)
-
-        if (users.any { it.username == username }) {
-            throw Exception("Username already exists")
-        }
-
-        val newUser = User(userId = users.size + 1, username = username, password = hashedPassword, userRole = userRole)
-        users.add(newUser)
-        authDataSource.saveAllUsers(users)
-        return newUser
-    }
-
-    override suspend fun loginUser(username: String, password: String): User {
-
-        checkUserNameAndPassword(username, password)
-        validPassword(password)
-
-        val hashedPassword = hashPassword(password)
-
-        val user =
-            users.find { it.username == username && it.password == hashedPassword } ?: throw Exception("User not found")
-        return user
-
-    }
-
-    override suspend fun getAllUsers(): List<User> {
-        return authDataSource.getAllUsers()
-    }
-
-    override suspend fun deleteUser(username: String): Boolean {
-        val user = users.find { it.username == username } ?: throw Exception("User not found")
-        users.remove(user)
-        authDataSource.saveAllUsers(users)
-        return true
-    }
-
-    override suspend fun editUser(user: User) {
-        val existingUser = users.find { it.username == user.username } ?: throw Exception("User not found")
-        users.remove(existingUser)
-        users.add(user)
-        authDataSource.saveAllUsers(users)
-    }
-
-    override suspend fun getUserByUserName(username: String): User? {
-        return users.find { it.username == username }
-    }
+    override suspend fun getAllUsers(): List<User> = authDataSource.getAllUsers()
 
 
-    private fun checkUserNameAndPassword(username: String, password: String) {
-        if (username.isEmpty() || password.isEmpty()) throw Exception("Username or password cannot be empty")
-    }
+    override suspend fun deleteUser(username: String): Boolean =  authDataSource.deleteUser(username)
 
-    private fun validPassword(password: String) {
-        if (password.length <= 6) throw Exception("Password must be at least 6 characters")
-    }
+    override suspend fun editUser(user: User) = authDataSource.editUser(user)
+
+    override suspend fun getUserByUserName(username: String): User? = authDataSource.getUserByUserName(username)
 
 }
--- src/main/kotlin/logic/repository/AuthRepository.kt
+++ src/main/kotlin/logic/repository/AuthRepository.kt
@@ -1,12 +1,13 @@
 package org.example.logic.repository
 
+import logic.request.auth.CreateUserRequest
 import org.example.logic.entity.auth.User
-import org.example.logic.entity.auth.UserRole
+import org.example.logic.request.auth.LoginRequest
 
 interface AuthRepository {
 
-    suspend fun insertUser(username: String, password: String, userRole: UserRole): User
-    suspend fun loginUser(username: String, password: String): User
+    suspend fun insertUser(request: CreateUserRequest): User
+    suspend fun loginUser(request: LoginRequest): User
     suspend fun getAllUsers(): List<User>
     suspend fun deleteUser(username: String): Boolean
     suspend fun editUser(user: User)
