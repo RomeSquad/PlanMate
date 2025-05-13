a9c9ff5b4585adafb760073ef81cc6153eb4dec7
Safa Thaaer <safathaaer@gmail.com>
2025-05-13 18:56:37 +0300

    add ProjectConstants for consistent project entication field references

---
+++ src/main/kotlin/data/utils/ProjectConstants.kt
@@ -0,0 +1,9 @@
+package org.example.data.utils
+
+object ProjectConstants {
+    const val PROJECT_ID = " projectId"
+    const val PROJECT_NAME = " name"
+    const val PROJECT_DESCRIPTION = "description"
+    const val PROJECT_STATE = "state"
+
+}
