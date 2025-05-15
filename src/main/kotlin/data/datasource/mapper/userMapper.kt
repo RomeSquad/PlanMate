3cb0d0f7648c7d9693251ff1f7f1fbb2a0ceb417
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-05-15 23:46:45 +0300

    add User Dto class

--- src/main/kotlin/data/datasource/mapper/userMapper.kt
+++ src/main/kotlin/data/datasource/mapper/userMapper.kt
@@ -1,26 +1,33 @@
 package org.example.data.datasource.mapper
 
+import data.datasource.authentication.dto.UserDto
 import logic.request.auth.CreateUserRequest
 import org.example.data.utils.hashStringWithMD5
 import org.example.logic.entity.auth.User
 import org.example.logic.entity.auth.UserRole
 import java.util.*
 
-fun User.toCsvRow(): List<String> {
+fun UserDto.toCsvRow(): List<String> {
     return listOf(userId.toString(), username, password, userRole.toString())
 }
 
 
-fun List<String>.fromCsvRowToUser(): User {
-    return User(
+fun List<String>.fromCsvRowToUser(): UserDto {
+    return UserDto(
         userId = UUID.fromString(this[0]),
         username = this[1],
         password = this[2],
         userRole = UserRole.valueOf(this[3])
     )
 }
+fun UserDto.toUser() = User(
+    userId = userId,
+    username = username,
+    userRole = userRole
+)
 
-fun CreateUserRequest.toUser() = User(
+
+fun CreateUserRequest.toUserDto() = UserDto(
     username = username,
     password = hashStringWithMD5(password),
     userRole = userRole
